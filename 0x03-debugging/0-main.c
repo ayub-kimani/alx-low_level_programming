de9b5e20c1ea82820c301cd0f5c8d3cb8b8b90f6
Ayub Kimani <ayubmbugua7@gmail.com>
2022-06-09 19:42:50 -0700

    Added 0-main.c file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,21 @@
+/*
+ * File: 101-print_number.c
+ * Author: Ayub Kimani
+ */
+
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
