d3b408c6859a85b5f136fc2430e90d8301dd6dbf
Ayub Kimani <ayubmbugua7@gmail.com>
2022-06-13 15:44:30 -0700

    Added 2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/*
+ * File: 2-strlen.c
+ * Author: Ayub Kimani
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
+}
