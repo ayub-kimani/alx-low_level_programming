99445df2d9b99fa799e73311447b2ac6b214178d
Ayub Kimani <ayubmbugua7@gmail.com>
2022-06-20 17:54:20 -0700

    Added 100-set_string.c file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+/*
+ * File: 100-set_string.c
+ * Author: Ayub Kimani
+ */
+
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: source adress.
+ * @to: target adress.
+ * Return: no return.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
