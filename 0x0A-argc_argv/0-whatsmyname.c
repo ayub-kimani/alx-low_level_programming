e503fb0324d866df87cfde23552d83cb8b3f5fba
Ayub Kimani <ayubmbugua7@gmail.com>
2022-06-24 19:45:59 -0700

    Added 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+/*
+ * File: 0-whatsmyname.c
+ * Auth: Ayub Kimani
+ */
+
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
