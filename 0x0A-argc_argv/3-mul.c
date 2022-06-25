54f01391e2cb7eb5b5dd3b077e0aa3037f4ec16e
Ayub Kimani <ayubmbugua7@gmail.com>
2022-06-24 19:51:12 -0700

    Added 3-mul.c file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+/*
+ * File: 3-mul.c
+ * Auth: Ayub Kimani
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
