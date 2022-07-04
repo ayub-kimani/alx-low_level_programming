554596df36f2bd06209a611e95026a474275004b
Ayub Kimani <ayubmbugua7@gmail.com>
2022-07-04 13:36:43 -0700

    Added 2-main.c file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+/*
+ * File: 2-main.c
+ * Auth: Ayub Kimani
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
