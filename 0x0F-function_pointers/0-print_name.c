bd867145e62279c59cf71455df84d0c2f5a4f350
Ayub Kimani <ayubmbugua7@gmail.com>
2022-07-06 14:29:02 -0700

    Added 0-print_name.c file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-print_name.c
+ * Auth: Ayub Kimani
+ */
+
+
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
