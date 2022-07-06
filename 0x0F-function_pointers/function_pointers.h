d610f4bec257ec0d75990cf88ce3c3b51f4f7a1e
Ayub Kimani <ayubmbugua7@gmail.com>
2022-07-06 14:50:57 -0700

    Added project header file

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,17 @@
+/*
+ * File: function_pointers.h
+ * AUth: Ayub Kimani
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0F-function_pointers directory.
+ */
+
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
