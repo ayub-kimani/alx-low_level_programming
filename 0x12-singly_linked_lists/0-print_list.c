b3b4e4ca61bd1a80c30846c0f05c1fbde519c027
Ayub Kimani <ayubmbugua7@gmail.com>
2022-07-14 16:17:39 -0700

    Updated 0-print_list.c file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,14 +16,18 @@
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
+
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
+
 		nodes++;
 		h = h->next;
 	}
+
 	return (nodes);
 }
