a1d8129a1b8e8e0d145a05af221f0a567d4dec51
Ayub Kimani <ayubmbugua7@gmail.com>
2022-07-19 15:43:24 -0700

    Added 8-sum_listint.c file

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+/*
+ * File: 8-sum_listint.c
+ * Auth: Ayub Kimani
+ */
+
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the data (n) of
+ * a linked list
+ * @head: head of a list
+ *
+ * Return: sum of all the data (n)
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
