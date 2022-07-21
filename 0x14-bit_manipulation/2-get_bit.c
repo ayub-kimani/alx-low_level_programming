143167a762762e5ad2e0fda0583dabb8ad84693a
Ayub Kimani <ayubmbugua7@gmail.com>
2022-07-21 15:23:36 -0700

    Added 2-get_bit.c file

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+/*
+ * File: 2-get_bit.c
+ * Auth: Ayub Kimani
+ */
+
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: unsigned long int input
+ * @index: index of the bit
+ *
+ * Return: value of the bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
+}
