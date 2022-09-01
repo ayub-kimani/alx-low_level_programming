0558ea2936601149068a15ddf1b7ae2246e68958
Ayub Kimani <ayubmbugua7@gmail.com>
2022-09-01 16:36:36 -0700

    Uploaded Task-1 List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,34 @@
+/*
+ * File: 1-dlistint_len.c
+ * Auth: Ayub Kimani
+ */
+
+#include "lists.h"
+
+/**
+ * dlistint_len - Counts the number of elements in a linked dlistint_t list.
+ * @h: The head of the dlistint_t list.
+ *
+ * Return: The number of elements in the dlistint_t list.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
